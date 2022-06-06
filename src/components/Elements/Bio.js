880f4ef908e9d13ccaed1410cef0fbe6f02ae723
Cole's Laptop <cole.webster@gmail.com>
2022-06-06 11:54:24 -0400

    Added clickable links

--- src/components/Elements/Bio.js
+++ src/components/Elements/Bio.js
@@ -7,26 +7,34 @@ export default function Bio() {
       </div>
 
       <div className="bio_contact_buttons">
-        <button className="bio_email">Email</button>
-        <button
+        <a className="bio_email">Email</a>
+        <a
           href="https://cwportfolio-app.herokuapp.com/"
           className="bio_portfolio"
         >
           Portfolio
-        </button>
+        </a>
+        <a
+          className="bio_resume"
+          href="https://drive.google.com/file/d/1jCQimcjPIqABnNZw71A7hM9b4V3t_PJa/view?usp=sharing"
+        >
+          Resume
+        </a>
       </div>
 
       <div className="bio_about_background">
         <h2>About</h2>
         <p className="bio">
-          Full stack web developer using background in sports and technology to build more user intuitive experiences online. 
+          Full stack web developer using background in sports and technology to
+          build more user intuitive experiences online.
           <br />
           <br />
-          I thrive in a team environment and have created three
-          mobile-first web applications built to solve real world problems and
-          keep the user engaged. Most recently, I worked on a team of five to
-          develop a full stack online implementation of classic table top party
-          game. Currently I am developing a full stack CRM application centered around a coffee shop and can't wait to show case more from this project.
+          I thrive in a team environment and have created three mobile-first web
+          applications built to solve real world problems and keep the user
+          engaged. Most recently, I worked on a team of five to develop a full
+          stack online implementation of classic table top party game. Currently
+          I am developing a full stack CRM application centered around a coffee
+          shop and can't wait to show case more from this project.
           <br />
           <br />
           I'm passionate about creating life-enhancing user experiences and
@@ -38,10 +46,12 @@ export default function Bio() {
 
         <h2>Skills</h2>
         <p className="skills">
-            Front End: HTML5, CSS, Bootstrap, JavaScript, jQuery, AJAX, React an TailWindCSS
-            <br />
-            <br />
-            Back End: Node.js, Express, MySQL, Sequelize ORM, MongoDB, Mongoose ODM, GraphQL
+          Front End: HTML5, CSS, Bootstrap, JavaScript, jQuery, AJAX, React an
+          TailWindCSS
+          <br />
+          <br />
+          Back End: Node.js, Express, MySQL, Sequelize ORM, MongoDB, Mongoose
+          ODM, GraphQL
         </p>
       </div>
     </>
--- src/components/Elements/Footer.js
+++ src/components/Elements/Footer.js
@@ -4,15 +4,24 @@ import { FaLinkedin, FaTwitterSquare, FaGithubSquare } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className="footer">
-      <i className="linkedin">
+      <a 
+        className="linkedin"
+        href="https://www.linkedin.com/in/colewebster/"
+      >
         <FaLinkedin size={56} />
-      </i>
-      <i className="twitter">
+      </a>
+      <a 
+        className="twitter"
+        href="https://twitter.com/Coding_CW"  
+      >
         <FaTwitterSquare size={56} />
-      </i>
-      <i className="github">
+      </a>
+      <a 
+        className="github"
+        href="https://github.com/ColeWebster"
+      >
         <FaGithubSquare size={56} />
-      </i>
+      </a>
     </div>
   );
 }
